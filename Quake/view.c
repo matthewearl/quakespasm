f1fb28b434c62e8ba55c764fd411ad98bfd53160
Andrei Drexler <makro@rq3.com>
2022-02-15 21:10:46 +0200

    Framerate-independent cshift decay

--- Quake/view.c
+++ Quake/view.c
@@ -489,6 +489,7 @@ V_UpdateBlend -- johnfitz -- V_UpdatePalette cleaned up and renamed
 void V_UpdateBlend (void)
 {
 	int		i, j;
+	float	frametime = cl.time - cl.oldtime;
 	qboolean	blend_changed;
 
 	V_CalcPowerupCshift ();
@@ -511,12 +512,12 @@ void V_UpdateBlend (void)
 	}
 
 // drop the damage value
-	cl.cshifts[CSHIFT_DAMAGE].percent -= host_frametime*150;
+	cl.cshifts[CSHIFT_DAMAGE].percent -= frametime*150;
 	if (cl.cshifts[CSHIFT_DAMAGE].percent <= 0)
 		cl.cshifts[CSHIFT_DAMAGE].percent = 0;
 
 // drop the bonus value
-	cl.cshifts[CSHIFT_BONUS].percent -= host_frametime*100;
+	cl.cshifts[CSHIFT_BONUS].percent -= frametime*100;
 	if (cl.cshifts[CSHIFT_BONUS].percent <= 0)
 		cl.cshifts[CSHIFT_BONUS].percent = 0;
 
