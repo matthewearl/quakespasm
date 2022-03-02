3d5dc57f81d7c66d31dce8689d4c0cf8ada81f8f
Andrei Drexler <makro@rq3.com>
2022-03-02 18:09:53 +0200

    Fix incorrect pitch when loading a savegame (issue #53)

--- Quake/host_cmd.c
+++ Quake/host_cmd.c
@@ -1669,7 +1669,10 @@ static void Host_Spawn_f (void)
 	ent = EDICT_NUM( 1 + (host_client - svs.clients) );
 	MSG_WriteByte (&host_client->message, svc_setangle);
 	for (i = 0; i < 2; i++)
-		MSG_WriteAngle (&host_client->message, ent->v.angles[i], sv.protocolflags );
+		if (sv.loadgame)
+			MSG_WriteAngle (&host_client->message, ent->v.v_angle[i], sv.protocolflags );
+		else
+			MSG_WriteAngle (&host_client->message, ent->v.angles[i], sv.protocolflags );
 	MSG_WriteAngle (&host_client->message, 0, sv.protocolflags );
 
 	SV_WriteClientdataToMessage (sv_player, &host_client->message);
