8e00060b8ec6e5ef326ce65076bdf31b2eeee2d2
Andrei Drexler <makro@rq3.com>
2022-03-31 21:48:57 +0300

    Don't autoplay next demo after a timedemo
    This happened when starting the game
    with either -fitz or cl_startdemos 1
    and then executing 'timedemo demo1'
    while a title demo was still playing

--- Quake/host.c
+++ Quake/host.c
@@ -146,7 +146,7 @@ void Host_EndGame (const char *message, ...)
 	if (cls.state == ca_dedicated)
 		Sys_Error ("Host_EndGame: %s\n",string);	// dedicated servers exit
 
-	if (cls.demonum != -1)
+	if (cls.demonum != -1 && !cls.timedemo)
 		CL_NextDemo ();
 	else
 		CL_Disconnect ();
