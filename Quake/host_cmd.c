91d630db72054414c90bc93d41f4e3e8bcfc36f8
Andrei Drexler <makro@rq3.com>
2022-06-10 19:26:14 +0300

    Don't spawn monsters when starting map with nomonsters 1 (#94)

--- Quake/host_cmd.c
+++ Quake/host_cmd.c
@@ -24,6 +24,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "quakedef.h"
 
 extern cvar_t	pausable;
+extern cvar_t	nomonsters;
 
 int	current_skill;
 
@@ -1037,6 +1038,12 @@ static void Host_Savegame_f (void)
 		return;
 	}
 
+	if (sv.nomonsters)
+	{
+		Con_Printf ("Can't save when using \"nomonsters\".\n");
+		return;
+	}
+
 	if (cl.intermission)
 	{
 		Con_Printf ("Can't save in intermission.\n");
@@ -1146,6 +1153,12 @@ static void Host_Loadgame_f (void)
 	{
 		Con_Printf ("Relative pathnames are not allowed.\n");
 		return;
+	}
+
+	if (nomonsters.value)
+	{
+		Con_Warning ("\"%s\" disabled automatically.\n", nomonsters.name);
+		Cvar_SetValueQuick (&nomonsters, 0.f);
 	}
 
 	cls.demonum = -1;		// stop demo loop in case this fails
