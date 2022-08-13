2071db51751c74e397cbea35b6ae8c31ebfd2a03
Andrei Drexler <makro@rq3.com>
2022-08-13 20:40:21 +0300

    Save cl_rollangle and cl_bob to config (#119)

--- Quake/view.c
+++ Quake/view.c
@@ -37,9 +37,9 @@ cvar_t	scr_ofsy = {"scr_ofsy","0", CVAR_NONE};
 cvar_t	scr_ofsz = {"scr_ofsz","0", CVAR_NONE};
 
 cvar_t	cl_rollspeed = {"cl_rollspeed", "200", CVAR_NONE};
-cvar_t	cl_rollangle = {"cl_rollangle", "2.0", CVAR_NONE};
+cvar_t	cl_rollangle = {"cl_rollangle", "2.0", CVAR_ARCHIVE};
 
-cvar_t	cl_bob = {"cl_bob","0.02", CVAR_NONE};
+cvar_t	cl_bob = {"cl_bob","0.02", CVAR_ARCHIVE};
 cvar_t	cl_bobcycle = {"cl_bobcycle","0.6", CVAR_NONE};
 cvar_t	cl_bobup = {"cl_bobup","0.5", CVAR_NONE};
 
