e341e4e6742228001ea2275d29cd10744eb654c0
Andrei Drexler <makro@rq3.com>
2023-02-06 01:34:58 +0200

    Remove commented-out declaration

--- Quake/gl_draw.c
+++ Quake/gl_draw.c
@@ -25,8 +25,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "quakedef.h"
 
-//extern unsigned char d_15to8table[65536]; //johnfitz -- never used
-
 cvar_t		scr_conalpha = {"scr_conalpha", "0.5", CVAR_ARCHIVE}; //johnfitz
 cvar_t		scr_conbrightness = {"scr_conbrightness", "1.0", CVAR_ARCHIVE};
 
